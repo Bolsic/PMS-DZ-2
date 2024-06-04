4c73f4ddf19f5dd5e08c6482c50beb1e90bf2d6b
Vladan Bašić <vladanbasic@gmail.com>
2024-06-04 18:26:58 +0200

    Pocetak kalibracije

--- fajl.py
+++ fajl.py
@@ -26,8 +26,16 @@ class App(QWidget):
 
     def __init__(self):
         super().__init__()
+        self.ser = serial.Serial('/dev/ttyACM0', 9600)
         self.title = 'Proba'
         self.left = 200
+
+        self.calibrationParams = {
+            'X': [0, 0],
+            'Y': [0, 0],
+            'Z': [0, 0]
+        }
+
         self.top = 200
         self.width = 730
         self.height = 520
@@ -41,7 +49,7 @@ class App(QWidget):
         self.calibrateButton = QPushButton('Calibrate', self)
         self.calibrateButton.resize(100, 50)
         self.calibrateButton.move(10, 10)
-        self.calibrateButton.clicked.connect(self.calibrate)
+        self.calibrateButton.clicked.connect(self.calibrateInterface)
 
         # Start Flight button
         self.startFlightButton = QPushButton('Start Flight', self)
@@ -62,29 +70,84 @@ class App(QWidget):
 
         self.show()
 
-    def calibrate(self):
-        # Calibration x button
-        self.calibrateButton = QPushButton('Calibrate X', self)
-        self.calibrateButton.resize(100, 50)
-        self.calibrateButton.move(100, 100)
-        self.calibrateButton.clicked.connect(self.calibrate)
+    def calibrateInterface(self):
+        # Calibrate X button
+        self.buttonX = QPushButton('X', self)
+        self.buttonX.resize(100, 50)
+        self.buttonX.move(10, 250)
+        self.buttonX.clicked.connect(self.calibrateX)
+        self.buttonX.show()
+        
+        # Calibrate Y button
+        self.buttonY = QPushButton('Y', self)
+        self.buttonY.resize(100, 50)
+        self.buttonY.move(10, 310)
+        self.buttonY.clicked.connect(self.calibrateY)
+        self.buttonY.show()
 
-        # Calibration y button
-        self.calibrateButton = QPushButton('Calibrate Y', self)
-        self.calibrateButton.resize(100, 50)
-        self.calibrateButton.move(100, 160)
-        self.calibrateButton.clicked.connect(self.calibrate)
+        # Calibrate Z button
+        self.buttonZ = QPushButton('Z', self)
+        self.buttonZ.resize(100, 50)
+        self.buttonZ.move(10, 370)
+        self.buttonZ.clicked.connect(self.calibrateZ)
+        self.buttonZ.show()
 
-        # Calibration z button
-        self.calibrateButton = QPushButton('Calibrate Z', self)
-        self.calibrateButton.resize(100, 50)
-        self.calibrateButton.move(100, 220)
-        self.calibrateButton.clicked.connect(self.calibrate)
-
-
-        print("CALIBRATING")
+        # End Calibration button
+        self.buttonEndCalibration = QPushButton('End Calibration', self)
+        self.buttonEndCalibration.resize(100, 50)
+        self.buttonEndCalibration.move(10, 430)
+        self.buttonEndCalibration.clicked.connect(self.endCalibration)
+        self.buttonEndCalibration.show()
         self.show()
+
+    def calibrateX(self):
+        self.calibrateCoordinate("X")
+
+    def calibrateY(self):
+        self.calibrateCoordinate("Y")
     
+    def calibrateZ(self):
+        self.calibrateCoordinate("Z")        
+
+    def calibrateCoordinate(self, coordinate):
+        print("Calibrating " + coordinate + "...")
+        # TODO: Implement calibration
+
+        # Send signal to Arduino to start calibration
+        self.ser.write(('calibrate-' + coordinate + '\n').encode())
+        dataArray = []
+        
+        # Read data from Arduino TWICE
+        for merenje in range(2):
+            dataArray.append(np.array([]))
+            start_time = time.time()
+            i = 0
+            while time.time() - start_time < 3 or i < 50:
+                data = self.ser.readline()
+                data = data.decode('utf-8')
+                dataArray[merenje].append(float(data))
+                i += 1
+
+            # Calculate mean value of the data
+            dataArray[merenje] = np.mean(dataArray)
+
+        # Calculate k and n
+        k = 19.62 / (dataArray[0] - dataArray[1])
+        n = -9.81 - k * dataArray[1]
+
+        # Save k and n to calibrationParams
+        self.calibrationParams[coordinate] = [k, n]
+
+    
+    def endCalibration(self):
+        # Remove X, Y, Z and End Calibration buttons
+        self.buttonX.deleteLater()
+        self.buttonY.deleteLater()
+        self.buttonZ.deleteLater()
+        self.buttonEndCalibration.deleteLater()
+
+        # TODO: Save the name of the pilot and calibration paramaters to a file
+
     def startFlight(self):
         print("LETIM")
 
